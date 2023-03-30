c3fbd9b868b67adb9d340d4b593d7d49c2b3e7c9
takaxis2 <axis1130@gmail.com>
2023-03-31 03:18:56 +0900

    port and build

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(80);
-  // await app.listen(3300);
+  // await app.listen(80);
+  await app.listen(3300);
 }
 bootstrap();
